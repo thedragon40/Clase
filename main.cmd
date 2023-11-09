900d55c012193cccfeab8963bd975681d9a8fb5f
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-09 14:08:45 +0100

    Update main.cmd

--- main.cmd
+++ main.cmd
@@ -1,6 +1,9 @@
 @echo off
-echo Abriendo ventanas...
+echo Iniciando "broma"...
 
 :start
-start "" cmd /c echo Ventana && timeout /nobreak /t 30 >nul
-goto start
+start "" cmd /c echo Ventana && timeout /nobreak /t 2 >nul
+timeout /nobreak /t 30 >nul
+taskkill /f /im cmd.exe
+
+echo Has sido "bromeado". && pause
