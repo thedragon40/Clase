c30939821c9794aea97a9cb1733c606608d72eec
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-09 14:04:44 +0100

    Update main.cmd

--- main.cmd
+++ main.cmd
@@ -2,6 +2,5 @@
 echo Abriendo ventanas...
 
 :start
-start "" cmd /c echo Ventana && timeout /nobreak /t 1 >nul
-timeout /nobreak /t 30 >nul
-taskkill /f /im cmd.exe
+start "" cmd /c echo Ventana && timeout /nobreak /t 30 >nul
+goto start
