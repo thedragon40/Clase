c12a4b5c7f0429d720cc7f604b48ea60c0156c92
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-10 13:34:06 +0100

    Update main.cmd

--- main.cmd
+++ main.cmd
@@ -1,16 +1,13 @@
 @echo off
 chcp 65001 > nul
-REM Lista de mensajes graciosos
-setlocal enabledelayedexpansion
-set mensajes[0]=^>¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.
-set mensajes[1]=^>¿Qué le dice un jardinero a otro jardinero? Nos vemos cuando podamos.
-set mensajes[2]=^>¿Por qué el libro de matemáticas se suicidó? Porque tenía demasiados problemas.
-set mensajes[3]=^>¿Qué hace una abeja en el gimnasio? ¡Zum-ba!
-set mensajes[4]=^>¿Cómo se llama el campeón de buceo japonés? Tokofondo.
-set mensajes[5]=^>¿Qué le dijo una iguana a su hermana gemela? Somos iguanitas.
-set mensajes[6]=^>¿Cómo se despiden los químicos? Ácido un placer.
 
 REM Crear múltiples ventanas de CMD y mostrar mensajes en ellas
 for /l %%i in (1, 1, 30) do (
     start cmd /k "!mensajes[%%i]!"
 )
+
+REM Esperar 10 segundos
+timeout /t 10 /nobreak
+
+REM Cerrar todas las ventanas de CMD
+taskkill /f /im cmd.exe
