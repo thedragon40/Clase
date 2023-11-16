e2c9f64ea58eaf12beb37d92dae45015754b6908
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-16 13:59:36 +0100

    Update main.cmd

--- main.cmd
+++ main.cmd
@@ -1,13 +1,10 @@
 @echo off
 chcp 65001 > nul
 
-REM Crear múltiples ventanas de CMD y mostrar mensajes en ellas
 for /l %%i in (1, 1, 30) do (
     start cmd /k
 )
 
-REM Esperar 10 segundos
 timeout /t 10 /nobreak
 
-REM Cerrar todas las ventanas de CMD
 taskkill /f /im cmd.exe
