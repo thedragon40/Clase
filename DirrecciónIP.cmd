5e959e014f614bb376ce7aba8fa2201a9a4d1fe9
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-17 13:09:22 +0100

    Update and rename DirrecciónIP to DirrecciónIP.cmd

--- DirrecciónIP.cmd
+++ DirrecciónIP.cmd
@@ -1,48 +1,54 @@
 @echo off
 
+:INICIO
 set /p IP="Ingrese la dirección IP base: "
 set /p Subredes="Ingrese el número de subredes: "
 
-set /a BitsRed=24 
-set /a Hosts=28
+set /a BitsRed=24
+set /a Hosts=28 
 
 setlocal EnableDelayedExpansion
 
 set /a BitsSubred=32-%BitsRed%
 set /a NumHosts=2^%BitsSubred%-2
 
-set /a TotHosts=%NumHosts% * %Subredes%
+set /a TotHosts=%NumHosts% * %Subredes% 
 
 if %TotHosts% GTR 254 (
   echo No hay suficientes hosts disponibles
-  goto :FIN
+  goto INICIO
 )
 
 set /a IP_Inicial=IP
 
 for /L %%i in (1,1,%Subredes%) do (
 
-  set /a Red=IP_Inicial
+  set /a Red=IP_Inicial  
   set /a Red=Red>>BitsRed
   set /a Red=Red<<BitsRed
-  
+   
   set /a Broad=Red+NumHosts
   set /a PriIP=Red+1
   set /a UltIP=Broad-1
-  
+   
   set /a TotIPs=NumHosts+2
-  
+   
   echo Subred %%i
   echo ========
-  echo IP de red:        !Red!
-  echo IP Primera:       !PriIP! 
-  echo IP Ultima:        !UltIP!
+  echo IP de red:        !Red! 
+  echo IP Primera:       !PriIP!
+  echo IP Ultima:        !UltIP! 
   echo IP Broadcast:     !Broad!
-  
+   
   set /a IP_Inicial+=NumHosts+2
   
 )
 
-:FIN
+:MENU
+echo.
+set /p opc=Desea volver a calcular? [0=Si, 1=No] 
 
-pause
+if %opc%==0 goto INICIO
+if %opc%==1 exit
+
+goto MENU
