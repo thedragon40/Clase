37a83f0b92f91b3d5617ed0da7c681521c17edbd
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-11-20 16:41:57 +0100

    Update Limpieza.cmd

--- Limpieza.cmd
+++ Limpieza.cmd
@@ -2,99 +2,127 @@
 chcp 65001 > nul
 title Mantenimiento del PC
 color 0E
-cls
+cls 
 
-echo Bienvenido al Asistente de Mantenimiento 
-echo Este programa realizará tareas de  
-echo optimización en su equipo.
+:: Mensaje inicial  
+color 0B
+echo Este asistente realizará tareas de 
+echo optimizacion y mantenimiento en su 
+echo equipo, como:
+echo.
+echo - Limpieza de archivos temporales
+echo - Analisis de disco  
+echo - Desfragmentacion
+echo - Optimizacion del sistema
+echo.
+echo Presione una tecla para continuar.
+pause >nul
 
-set /p continuar=Presione ENTER para continuar o Ctrl+C para cancelar.
+:: Comprobar permisos 
 
-:: Comprobar permisos de administrador  
-openfiles >nul 2>&1 || (
-  echo Requiere permisos de administrador.
-  powershell start %0 '-verb runas' 
-  exit /b  
+net session >nul 2>&1
+if %errorLevel% == 0 (
+  echo Permisos de administrador OK.
+) else (
+  color 0C
+  echo Requiere ejecucion como administrador.
+  timeout /t 5 >nul
+  exit
+)  
+
+:: Detectar SO
+color 0D 
+for /f "tokens=4-5 delims=. " %%i in ('ver') do (
+  set version=%%i.%%j
+  echo Sistema Operativo Windows %%i.%%j detectado. 
 )
 
-:: Detectar sistema operativo
-for /f "tokens=4-5 delims=. " %%i in ('ver') do set version=%%i.%%j
-if "%version%"=="5.1" echo Windows XP && goto :XP
-if "%version%"=="6.0" echo Windows Vista && goto :Vista
-if "%version%"=="6.1" echo Windows 7 && goto :Win7
-if "%version%"=="6.2" echo Windows 8 && goto :Win8
-if "%version%"=="6.3" echo Windows 8.1 && goto :Win81
-if "%version%"=="10.0" echo Windows 10 && goto :Win10
-echo Sistema operativo no compatible. && goto :EOF
+:: Comandos segun SO
+if "%version%"=="5.1" goto :XP
+if "%version%"=="6.0" goto :Vista
+if "%version%"=="6.1" goto :Win7
+if "%version%"=="6.2" goto :Win8
+if "%version%"=="6.3" goto :Win81
+if "%version%"=="10.0" goto :Win10
+goto :Win11
 
 :XP
+color 0E
 del %temp%\* /s /f /q
 ipconfig /flushdns
 netsh int ip reset
 goto :finalizar
 
-:Vista
-cleanmgr /sageset:99  
+:Vista  
+color 0B
 cleanmgr /sagerun:99
-diskpart /s limpiar todo
+diskpart /s limpiar todo  
+DISM /Online /Cleanup-Image /RestoreHealth
+chkdsk /f
 goto :finalizar
 
 :Win7
-cleanmgr /verylowdisk
-chkdsk /f  
-sfc /scannow
-defrag C: 
-goto :finalizar
-
-:Win8
-Dism /Cleanup-Image /StartComponentCleanup 
-chkdsk /spotfix
-goto :finalizar 
-
-:Win81  
+color 0D
 cleanmgr /verylowdisk
 chkdsk /f 
 sfc /scannow
-Dism /Online /Cleanup-Image /RestoreHealth
+defrag C:
+DISM /Online /Cleanup-Image /Restorehealth
 goto :finalizar
 
-:Win10
-cleanmgr /verylowdisk
-chkdsk /f  
-sfc /scannow  
+:Win8
+color 0C  
+Dism /Cleanup-Image /StartComponentCleanup
+chkdsk /spotfix
 DISM /Online /Cleanup-Image /RestoreHealth
-defrag C: /U /V
+cleanmgr /verylowdisk
 goto :finalizar
 
-:Win11 
-cleanmgr /verylowdisk
+:Win81 
+color 0E
+cleanmgr /verylowdisk 
 chkdsk /f
 sfc /scannow
-DISM /Online /Cleanup-Image /RestoreHealth  
+DISM /Online /Cleanup-Image /RestoreHealth
 defrag C: /U /V  
 goto :finalizar
 
+:Win10
+color 0A
+cleanmgr /verylowdisk  
+chkdsk /f 
+sfc /scannow
+DISM /Online /Cleanup-Image /RestoreHealth  
+defrag C: /U /V
+goto :finalizar
+
+:Win11
+color 0F
+cleanmgr /verylowdisk
+chkdsk /f  
+sfc /scannow  
+DISM /Online /Cleanup-Image /Restorehealth
+defrag C: /U /V
+goto :finalizar
+
 :finalizar
-:: Mostrar barra de progreso 
+
+:finalizar 
+color 0A
+:: Mostrar progreso
 for /L %%A in (1,1,100) do (
   cls
   echo Progreso: [%%A%%%]
   ping -n 2 localhost >nul 
 )
 
-:: Preguntar por programación automática
-set /p programar=¿Desea programar el mantenimiento automático? (S/N): 
-if /i "%programar%"=="S" (
-  set /p intervalo=¿Cada cuantos días?  
-  schtasks /create /sc daily /mo %intervalo% /tn "Mantenimiento" /tr "mantenimiento.cmd"
-  echo Programado para cada %intervalo% días. 
-)
-
-:: Preguntar por reinicio  
+:: Preguntas finales 
+color 0C
 set /p reiniciar=¿Desea reiniciar la PC ahora? (S/N)
 if /i "%reiniciar%"=="S" (
   shutdown /r /t 0
 )
 
-echo.
-echo Proceso finalizado.
+:: Mensaje final
+color 0F
+echo Mantenimiento completado.
