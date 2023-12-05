1c7b586c165883153eab34a02bc668aa5002cfe8
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-05 13:39:49 +0100

    Update Mante.ps1

--- Mante.ps1
+++ Mante.ps1
@@ -36,13 +36,28 @@ $lista = New-Object System.Windows.Forms.ListBox
 # Agrega los comandos de mantenimiento a los botones.
 $botonAnalizarSistema.Add_Click({$lista.SelectedItem | & "sfc /scannow"})
 $botonRepararArchivos.Add_Click({$lista.SelectedItem | & "sfc /scannow"})
-$botonLimpiarDiscoDuro.Add_Click({$lista.SelectedItem | & "cleanmgr"})
-$botonLiberarEspacio.Add_Click({$lista.SelectedItem | & "cleanmgr"})
-$botonDesfragmentarDiscoDuro.Add_Click({$lista.SelectedItem | & "defrag"})
+
+# Pestaña Limpiar
+$botonLimpiarDiscoDuro.Add_Click({$lista.SelectedItem | & "cleanmgr /dskcleanup"})
+$botonLiberarEspacio.Add_Click({$lista.SelectedItem | & "cleanmgr /sageset:1"})
+
+# Pestaña Optimizar
+$botonDesfragmentarDiscoDuro.Add_Click({$lista.SelectedItem | & "defrag /c /w"})
 $botonAjustarConfiguración.Add_Click({$lista.SelectedItem | & "sysdm.cpl"})
+
+# Pestaña Actualizar
 $botonInstalarActualizaciones.Add_Click({$lista.SelectedItem | & "wuauclt /updatenow"})
 $botonVerificarEstado.Add_Click({$lista.SelectedItem | & "wuauclt /checknow"})
 
+# Agrega los comandos de mantenimiento a la lista.
+$lista.Items.Add("Analizar el sistema")
+$lista.Items.Add("Reparar archivos dañados")
+$lista.Items.Add("Limpiar el disco duro")
+$lista.Items.Add("Liberar espacio en el disco duro")
+$lista.Items.Add("Desfragmentar el disco duro")
+$lista.Items.Add("Ajustar la configuración del sistema")
+$lista.Items.Add("Instalar actualizaciones")
+$lista.Items.Add("Verificar el estado de las actualizaciones")
 
 # Muestra el formulario.
 $form.ShowDialog()
