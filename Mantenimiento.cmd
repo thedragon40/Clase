29285ca4c7289b88fe2174023fdeacbc210dc2c9
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-05 10:14:06 +0100

    Update Mantenimiento.cmd

--- Mantenimiento.cmd
+++ Mantenimiento.cmd
@@ -1,199 +1,51 @@
 @echo off
-chcp 65001 > nul  
-setlocal EnableExtensions EnableDelayedExpansion
+title Herramienta Completa de Mantenimiento
 
-:: Comprobación de privilegios elevados
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-  echo Este script requiere privilegios administrativos. Ejecútelo como administrador.
-  pause
-  exit /b 1
-)
+powershell.exe -noexit -c "& {
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
-echo 1. Limpiar archivos temporales
-echo 2. Limpiar archivos temporales de Internet Explorer
-echo 3. Limpiar archivos temporales de actualizaciones de Windows
-echo 4. Desfragmentar y optimizar unidades
-echo 5. Comprobar y reparar errores en la unidad del sistema
-echo 6. Eliminar archivos temporales del sistema de Windows
-echo 7. Limpiar la caché de archivos temporales de Internet Explorer
-echo 8. Desactivar el seguimiento de usuario de Windows
-echo 9. Eliminar historial de navegación de Internet Explorer
-echo 10. Todos los anteriores
-echo 11. Salir
-echo.
-set /p option= Seleccione una opcion:
+function Ejecutar-Acciones {
+    $accionesSeleccionadas = $listbox.CheckedItems
 
-if %option%==1 goto :Option1
-if %option%==2 goto :Option2
-if %option%==3 goto :Option3
-if %option%==4 goto :Option4
-if %option%==5 goto :Option5
-if %option%==6 goto :Option6
-if %option%==7 goto :Option7
-if %option%==8 goto :Option8
-if %option%==9 goto :Option9
-if %option%==10 goto :OptionAll
-if %option%==11 goto :Exit
+    foreach ($accion in $accionesSeleccionadas) {
+        # Agrega lógica para ejecutar cada acción seleccionada
+        Write-Host 'Ejecutando acción:' $accion
+    }
+}
 
-:Option1
-echo.
-echo Limpiando archivos temporales...
-echo.
-del /s /q %temp%\*.*
-del /s /q %windir%\Temp\*.*
-echo.
-echo Limpieza de archivos temporales finalizada.
-echo.
-pause
-exit
+$form = New-Object Windows.Forms.Form 
+$form.Size = '500,500'
+$form.Text = 'Herramienta Completa de Mantenimiento'
 
-:Option2
-echo.
-echo Limpiando archivos temporales de Internet Explorer...
-echo.
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
-echo.
-echo Limpieza de archivos temporales de Internet Explorer finalizada.
-echo.
-pause
-exit
+$listbox = New-Object Windows.Forms.CheckedListBox
+$listbox.Location = '20,50'
+$listbox.Size = '180,280'
+$listbox.CheckOnClick = $true
 
-:Option3
-echo.
-echo Limpiando archivos temporales de actualizaciones de Windows...
-echo.
-del /s /q %windir%\SoftwareDistribution\Download\*.*
-echo.
-echo Limpieza de archivos temporales de actualizaciones de Windows finalizada.
-echo.
-pause
-exit
+$listbox.Items.Add('Limpiar archivos temporales', 0)
+$listbox.Items.Add('Limpiar Caché de DNS', 0)  
+$listbox.Items.Add('Limpiar errores de sistema', 0)
+$listbox.Items.Add('Desactivar inicio de Apps innecesarias', 0)
+$listbox.Items.Add('Desfragmentar disco', 0)  
+$listbox.Items.Add('Eliminar archivos innecesarios', 0)
+$listbox.Items.Add('Detectar archivos dañados', 0)
+$listbox.Items.Add('Chequear errores en Disco', 0)
+$listbox.Items.Add('Liberar memoria caché', 0)
+$listbox.Items.Add('Optimizar tiempo de inicio', 0)
+$listbox.Items.Add('Analizar consumo de recursos', 0)
 
-:Option4
-echo.
-echo Selecciona una unidad para desfragmentar y optimizar:
-echo.
-call :ListDrives
-set /p drive= Seleccione una unidad o escriba "Todas": 
+$button = New-Object Windows.Forms.Button
+$button.Text = "Ejecutar"
+$button.Location = '250,230'  
+$button.Add_Click({
+    Ejecutar-Acciones
+}) 
 
-if /i "%drive%"=="Todas" goto :DefragAll
-if not exist %drive% goto :InvalidDrive
+$form.Controls.Add($listbox)
+$form.Controls.Add($button)
+$form.Topmost = $true 
 
-echo.
-echo Desfragmentando y optimizando unidad %drive%...
-echo.
-defrag %drive% /U /V
-echo.
-echo Desfragmentacion y optimizacion de unidad %drive% finalizada.
-echo.
-pause
-exit
-
-:DefragAll
-echo.
-echo Desfragmentando y optimizando todas las unidades...
-echo.
-for /f "tokens=2" %%i in ('wmic logicaldisk get caption') do (
-  defrag %%i /U /V
-)
-echo.
-echo Desfragmentacion y optimizacion de todas las unidades finalizada.
-echo.
-pause
-exit
-
-:InvalidDrive
-echo.
-echo Unidad no válida. Por favor, seleccione una unidad válida.
-echo.
-pause
-exit
-
-:Option5
-echo.
-echo Comprobando y reparando errores en la unidad del sistema...
-echo.
-sfc /scannow
-echo.
-echo Comprobacion y reparacion de errores en la unidad del sistema finalizada.
-echo.
-pause
-exit
-
-:Option6
-echo.
-echo Eliminando archivos temporales del sistema de Windows...
-echo.
-del /s /q %windir%\Prefetch\*.*
-del /s /q %windir%\Temp\*.*
-echo.
-echo Eliminacion de archivos temporales del sistema de Windows finalizada.
-echo.
-pause
-exit
-
-:Option7
-echo.
-echo Limpiando la caché de archivos temporales de Internet Explorer...
-echo.
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
-echo.
-echo Limpieza de la caché de archivos temporales de Internet Explorer finalizada.
-echo.
-pause
-exit
-
-:Option8
-echo.
-echo Desactivando el seguimiento de usuario de Windows...
-echo.
-reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d "0" /f
-echo.
-echo Desactivacion del seguimiento de usuario de Windows finalizada.
-echo.
-pause
-exit
-
-:Option9
-echo.
-echo Eliminando historial de navegación de Internet Explorer...
-echo.
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1
-echo.
-echo Eliminacion del historial de navegacion de Internet Explorer finalizada.
-echo.
-pause
-exit
-
-:OptionAll
-echo.
-echo Ejecutando todas las opciones anteriores...
-echo.
-
-:: Incluye aquí los comandos de cada opción individual excepto la eliminación de archivos de descargas.
-del /s /q %temp%\*.*
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
-del /s /q %windir%\SoftwareDistribution\Download\*.*
-sfc /scannow
-del /s /q %windir%\Prefetch\*.*
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
-reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d "0" /f
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1
-
-echo.
-echo Todas las anteriores ejecutadas.
-echo.
-pause
-exit
-
-:Exit
-exit
-
-:ListDrives
-echo.
-for /f "tokens=2" %%i in ('wmic logicaldisk get caption') do (
-  echo %%i
-)
-echo.
-exit /b
+$form.Add_Shown({$form.Activate()})
+[void] $form.ShowDialog()
+}";
