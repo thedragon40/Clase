db7865d4864279e5741e1a25ca1dc9929022d6d3
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-05 14:11:44 +0100

    Update Mante.ps1

--- Mante.ps1
+++ Mante.ps1
@@ -9,7 +9,7 @@ if (-not ([Security.Principal.WindowsPrincipal]::GetCurrent().IsInRole([Security
 # Declarar variables
 $selectedFunctions = ""
 
-# Mostrar lista de funciones
+# Mostrar lista de funciones 
 Write-Host "Funciones disponibles:"
 Write-Host "1. Actualizar Windows"
 Write-Host "2. Buscar y eliminar virus y malware"
@@ -25,7 +25,7 @@ Write-Host "9. Reiniciar el sistema"
 Write-Host "Escribe los números de las funciones que deseas ejecutar (separadas por espacios): "
 $selectedFunctions = Read-Host
 
-# Validar selección
+# Validar selección 
 if ($selectedFunctions -eq "") {
     Write-Host "Error: Selecciona al menos una función."
     exit
@@ -35,50 +35,59 @@ if ($selectedFunctions -eq "") {
 $selectedFunctionsArray = $selectedFunctions.Split(" ")
 foreach ($function in $selectedFunctionsArray) {
     switch ($function) {
-        case "1":
-            # Actualizar Windows
-            Write-Host "Actualizando Windows..."
-            Start-Process "wuauclt.exe" -ArgumentList "/updatenow"
+        case "1": 
+            {
+                Write-Host "Actualizando Windows..."
+                Start-Process "wuauclt.exe" -ArgumentList "/updatenow"
+            }
             break
         case "2":
-            # Buscar y eliminar virus y malware
-            Write-Host "Buscando y eliminando virus y malware..."
-            Start-Process "msconfig.exe" -ArgumentList "/scannow"
+            {
+                Write-Host "Buscando y eliminando virus y malware..." 
+                Start-Process "msconfig.exe" -ArgumentList "/scannow"
+            }
             break
         case "3":
-            # Liberar espacio en disco
-            Write-Host "Liberando espacio en disco..."
-            Start-Process "cleanmgr.exe" -ArgumentList "/sageset:65535 /d %windir%\System32\config\systemprofile\AppData\Local\Temp /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\Temporary Internet Files /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\SoftwareDistribution\Download"
+            {
+                Write-Host "Liberando espacio en disco..."
+                Start-Process "cleanmgr.exe" -ArgumentList "/sageset:65535 /d %windir%\System32\config\systemprofile\AppData\Local\Temp /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\Temporary Internet Files /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\SoftwareDistribution\Download"
+            }
             break
         case "4":
-            # Optimizar el registro de Windows
-            Write-Host "Optimizando el registro de Windows..."
-            Start-Process "regedit.exe" -ArgumentList "/s %~dp0\optimize_registry.reg"
+            {
+                Write-Host "Optimizando el registro de Windows..."
+                Start-Process "regedit.exe" -ArgumentList "/s %~dp0\optimize_registry.reg"
+            }
             break
         case "5":
-            # Desfragmentar el disco duro
-            Write-Host "Desfragmentando el disco duro..."
-            Start-Process "dfrg.msc"
+            {
+                Write-Host "Desfragmentando el disco duro..." 
+                Start-Process "dfrg.msc"
+            }
             break
         case "6":
-            # Reparar archivos de sistema
-            Write-Host "Reparando archivos de sistema..."
-            Start-Process "sfc.exe" -ArgumentList "/scannow"
+            {
+                Write-Host "Reparando archivos de sistema..."
+                Start-Process "sfc.exe" -ArgumentList "/scannow"
+            }
             break
         case "7":
-            # Eliminar archivos temporales
-            Write-Host "Eliminando archivos temporales..."
-            Start-Process "cmd.exe" -ArgumentList "/c del /q /f %windir%\Temp\*"
+            {
+                Write-Host "Eliminando archivos temporales..."
+                Start-Process "cmd.exe" -ArgumentList "/c del /q /f %windir%\Temp\*"  
+            }
             break
         case "8":
-            # Desinstalar programas no deseados
-            Write-Host "Desinstalando programas no deseados..."
-            Start-Process "appwiz.cpl"
+            {
+                Write-Host "Desinstalando programas no deseados..."
+                Start-Process "appwiz.cpl"
+            }
             break
-        case "9":
-            # Reiniciar el sistema
-            Write-Host "Reiniciando el sistema..."
-            Start-Process "shutdown.exe" -ArgumentList "/r /t 0"
+        case "9": 
+            {
+                Write-Host "Reiniciando el sistema..."
+                Start-Process "shutdown.exe" -ArgumentList "/r /t 0"
+            }
             break
         default:
             Write-Host "Función no válida."
