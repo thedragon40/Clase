7bee77ac1043a889751fcb29415e45be0677f6ba
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-05 14:08:30 +0100

    Update Mante.ps1

--- Mante.ps1
+++ Mante.ps1
@@ -36,38 +36,47 @@ $selectedFunctionsArray = $selectedFunctions.Split(" ")
 foreach ($function in $selectedFunctionsArray) {
     switch ($function) {
         case "1":
+            # Actualizar Windows
             Write-Host "Actualizando Windows..."
             Start-Process "wuauclt.exe" -ArgumentList "/updatenow"
             break
         case "2":
+            # Buscar y eliminar virus y malware
             Write-Host "Buscando y eliminando virus y malware..."
             Start-Process "msconfig.exe" -ArgumentList "/scannow"
             break
         case "3":
+            # Liberar espacio en disco
             Write-Host "Liberando espacio en disco..."
             Start-Process "cleanmgr.exe" -ArgumentList "/sageset:65535 /d %windir%\System32\config\systemprofile\AppData\Local\Temp /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\Temporary Internet Files /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\SoftwareDistribution\Download"
             break
         case "4":
+            # Optimizar el registro de Windows
             Write-Host "Optimizando el registro de Windows..."
             Start-Process "regedit.exe" -ArgumentList "/s %~dp0\optimize_registry.reg"
             break
         case "5":
+            # Desfragmentar el disco duro
             Write-Host "Desfragmentando el disco duro..."
             Start-Process "dfrg.msc"
             break
         case "6":
+            # Reparar archivos de sistema
             Write-Host "Reparando archivos de sistema..."
             Start-Process "sfc.exe" -ArgumentList "/scannow"
             break
         case "7":
+            # Eliminar archivos temporales
             Write-Host "Eliminando archivos temporales..."
             Start-Process "cmd.exe" -ArgumentList "/c del /q /f %windir%\Temp\*"
             break
         case "8":
+            # Desinstalar programas no deseados
             Write-Host "Desinstalando programas no deseados..."
             Start-Process "appwiz.cpl"
             break
         case "9":
+            # Reiniciar el sistema
             Write-Host "Reiniciando el sistema..."
             Start-Process "shutdown.exe" -ArgumentList "/r /t 0"
             break
