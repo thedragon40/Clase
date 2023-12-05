628ec2fcb4a8cca09af631d41721eb9ccdc135a4
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-05 14:06:25 +0100

    Update Mante.ps1

--- Mante.ps1
+++ Mante.ps1
@@ -1,47 +1,77 @@
-# Este script crea una interfaz gráfica completa para realizar mantenimiento a Windows 10 o 11.
+# Script de mantenimiento de Windows
 
-# Importa los módulos necesarios.
-Import-Module Microsoft.PowerShell.Management
-Import-Module System.Windows.Forms
+# Ejecutar como administrador
+if (-not ([Security.Principal.WindowsPrincipal]::GetCurrent().IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+    Write-Host "Este script debe ejecutarse con privilegios de administrador."
+    exit
+}
 
-# Crea un nuevo formulario.
-$form = New-Form -WindowTitle "Mantenimiento de Windows"
+# Declarar variables
+$selectedFunctions = ""
 
-# Crea pestañas para las diferentes herramientas de mantenimiento.
-$pestañaAnalizar = $form.TabControl.Controls.AddTab("Analizar")
-$pestañaLimpiar = $form.TabControl.Controls.AddTab("Limpiar")
-$pestañaOptimizar = $form.TabControl.Controls.AddTab("Optimizar")
-$pestañaActualizar = $form.TabControl.Controls.AddTab("Actualizar")
+# Mostrar lista de funciones
+Write-Host "Funciones disponibles:"
+Write-Host "1. Actualizar Windows"
+Write-Host "2. Buscar y eliminar virus y malware"
+Write-Host "3. Liberar espacio en disco"
+Write-Host "4. Optimizar el registro de Windows"
+Write-Host "5. Desfragmentar el disco duro"
+Write-Host "6. Reparar archivos de sistema"
+Write-Host "7. Eliminar archivos temporales"
+Write-Host "8. Desinstalar programas no deseados"
+Write-Host "9. Reiniciar el sistema"
 
-# Crea botones para todas las herramientas de mantenimiento.
-# Pestaña Analizar
-$botonAnalizarSistema = $pestañaAnalizar.Controls.AddButton("Analizar el sistema")
-$botonRepararArchivos = $pestañaAnalizar.Controls.AddButton("Reparar archivos dañados")
+# Leer la selección del usuario
+Write-Host "Escribe los números de las funciones que deseas ejecutar (separadas por espacios): "
+$selectedFunctions = Read-Host
 
-# Pestaña Limpiar
-$botonLimpiarDiscoDuro = $pestañaLimpiar.Controls.AddButton("Limpiar el disco duro")
-$botonLiberarEspacio = $pestañaLimpiar.Controls.AddButton("Liberar espacio en el disco duro")
+# Validar selección
+if ($selectedFunctions -eq "") {
+    Write-Host "Error: Selecciona al menos una función."
+    exit
+}
 
-# Pestaña Optimizar
-$botonDesfragmentarDiscoDuro = $pestañaOptimizar.Controls.AddButton("Desfragmentar el disco duro")
-$botonAjustarConfiguración = $pestañaOptimizar.Controls.AddButton("Ajustar la configuración del sistema")
-
-# Pestaña Actualizar
-$botonInstalarActualizaciones = $pestañaActualizar.Controls.AddButton("Instalar actualizaciones")
-$botonVerificarEstado = $pestañaActualizar.Controls.AddButton("Verificar el estado de las actualizaciones")
-
-# Agrega los comandos de mantenimiento a los botones.
-$botonAnalizarSistema.Add_Click({$lista.SelectedItem | & "sfc /scannow"})
-$botonRepararArchivos.Add_Click({$lista.SelectedItem | & "sfc /scannow"})
-
-# Pestaña Limpiar
-$botonLimpiarDiscoDuro.Add_Click({$lista.SelectedItem | & "cleanmgr /dskcleanup"})
-$botonLiberarEspacio.Add_Click({$lista.SelectedItem | & "cleanmgr /sageset:1"})
-
-# Pestaña Optimizar
-$botonDesfragmentarDiscoDuro.Add_Click({$lista.SelectedItem | & "defrag /c /w"})
-$botonAjustarConfiguración.Add_Click({$lista.SelectedItem | & "sysdm.cpl"})
-
-# Pestaña Actualizar
-$botonInstalarActualizaciones.Add_Click({$lista.SelectedItem | & "wuauclt /updatenow"})
-$botonVerificarEstado.Add_Click({$lista.SelectedItem | & "wuauclt /checknow"})
+# Ejecutar las funciones seleccionadas
+$selectedFunctionsArray = $selectedFunctions.Split(" ")
+foreach ($function in $selectedFunctionsArray) {
+    switch ($function) {
+        case "1":
+            Write-Host "Actualizando Windows..."
+            Start-Process "wuauclt.exe" -ArgumentList "/updatenow"
+            break
+        case "2":
+            Write-Host "Buscando y eliminando virus y malware..."
+            Start-Process "msconfig.exe" -ArgumentList "/scannow"
+            break
+        case "3":
+            Write-Host "Liberando espacio en disco..."
+            Start-Process "cleanmgr.exe" -ArgumentList "/sageset:65535 /d %windir%\System32\config\systemprofile\AppData\Local\Temp /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\Temporary Internet Files /d %windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\SoftwareDistribution\Download"
+            break
+        case "4":
+            Write-Host "Optimizando el registro de Windows..."
+            Start-Process "regedit.exe" -ArgumentList "/s %~dp0\optimize_registry.reg"
+            break
+        case "5":
+            Write-Host "Desfragmentando el disco duro..."
+            Start-Process "dfrg.msc"
+            break
+        case "6":
+            Write-Host "Reparando archivos de sistema..."
+            Start-Process "sfc.exe" -ArgumentList "/scannow"
+            break
+        case "7":
+            Write-Host "Eliminando archivos temporales..."
+            Start-Process "cmd.exe" -ArgumentList "/c del /q /f %windir%\Temp\*"
+            break
+        case "8":
+            Write-Host "Desinstalando programas no deseados..."
+            Start-Process "appwiz.cpl"
+            break
+        case "9":
+            Write-Host "Reiniciando el sistema..."
+            Start-Process "shutdown.exe" -ArgumentList "/r /t 0"
+            break
+        default:
+            Write-Host "Función no válida."
+    }
+}
