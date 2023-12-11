dc086e332d9d9ca09986d14c0ff98b243b4efcc6
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-11 13:50:47 +0100

    Update miste.ps1

--- miste.ps1
+++ miste.ps1
@@ -13,18 +13,32 @@ if ($continue -eq 'S') {
 
     Write-Host -ForegroundColor Green "Iniciando hackeo..."
     Start-Sleep -Seconds 2
-    Write-Host -ForegroundColor Green "Extrayendo datos personales..." 
+    
+    Write-Host -ForegroundColor Green "Descifrando contraseñas..."
+    Start-Sleep -Seconds 1  
+    
+    Write-Host -ForegroundColor Green "Accediendo a cámara web..."
+    Start-Sleep -Seconds 1
+    
+    Write-Host -ForegroundColor Green "Monitorizando llamadas telefónicas..." 
+    Start-Sleep -Seconds 1
+
+    Write-Host -ForegroundColor Green "Leyendo historial de navegación..."
+    Start-Sleep -Seconds 1
+
+    Write-Host -ForegroundColor Green "Extrayendo fotos y archivos..." 
     Start-Sleep -Seconds 2
-    Write-Host -ForegroundColor Green "Injectando virus..."
-    Start-Sleep -Seconds 2
-    Write-Host -ForegroundColor Green "Enviando datos a servidor remoto..."
+    
+    Write-Host -ForegroundColor Green "Enviando datos a servidor secreto..."
     Start-Sleep -Seconds 2
 
-    Write-Host "Hackeo completado! Sus datos ahora me pertenecen!" -ForegroundColor Red
+    Write-Host -ForegroundColor Red "Todos sus datos me pertenecen ahora! Muajaja!"
+    
+    Write-Host -ForegroundColor Red "Le estaré vigilando..." 
 
 } else {
 
-    Write-Host "Operación cancelada"
+   Write-Host "Operación cancelada"
 
 }
 
