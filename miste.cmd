cdc1b554c999def695769d3fb7c559bc6762dd3f
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-11 13:53:39 +0100

    Update and rename miste.ps1 to miste.cmd

---
+++ miste.cmd
@@ -0,0 +1,39 @@
+@echo off
+cls
+
+set /p name="Introduzca su nombre: "
+set /p lastname="Introduzca su apellido: "
+set /p age="Introduzca su edad: "
+set /p personaldata="Introduzca algún dato personal: "
+set /p continue="¿Desea continuar? (S/N): "
+
+if %continue%==S goto hackeo
+if %continue%==s goto hackeo
+goto cancelado
+
+:hackeo
+cls 
+echo Iniciando hackeo...
+ping localhost -n 3 >nul
+echo Descifrando contraseñas... 
+ping localhost -n 2 >nul
+echo Accediendo a camara web...
+ping localhost -n 2 >nul  
+echo Monitorizando llamadas telefonicas...
+ping localhost -n 2 >nul
+echo Leyendo historial de navegacion...  
+ping localhost -n 2 >nul
+echo Extrayendo fotos y archivos...
+ping localhost -n 3 >nul
+echo Enviando datos a servidor secreto...
+ping localhost -n 3 >nul
+echo.
+echo ^<Todos sus datos me pertenecen ahora! Muajaja!^>
+echo ^<Le estare vigilando...^>
+goto finalizar
+
+:cancelado
+echo Operación cancelada.
+
+:finalizar 
+pause >nul
