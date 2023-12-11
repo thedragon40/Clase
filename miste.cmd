067477211a4b50f28eeb50acd9ee292d7dd09b27
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-11 14:09:14 +0100

    Update miste.cmd

--- miste.cmd
+++ miste.cmd
@@ -1,40 +1,43 @@
 @echo off
 chcp 65001 > nul
-cls
 
-set /p name="Introduzca su nombre: "
-set /p lastname="Introduzca su apellido: "
-set /p age="Introduzca su edad: "
-set /p personaldata="Introduzca algún dato personal: "
-set /p continue="¿Desea continuar? (S/N): "
+set "name="
+set "lastname="
+set "age="
+set "personaldata="
+set "continue="
 
-if %continue%==S goto hackeo
-if %continue%==s goto hackeo
+set /p "name=Introduzca su nombre: "
+set /p "lastname=Introduzca su apellido: "
+set /p "age=Introduzca su edad: "
+set /p "personaldata=Introduzca algún dato personal: "
+set /p "continue=¿Desea continuar? (S/N): "
+
+if /i "%continue%"=="s" goto hackeo
 goto cancelado
 
 :hackeo
-cls 
 echo Iniciando hackeo...
-ping localhost -n 3 >nul
-echo Descifrando contraseñas... 
-ping localhost -n 2 >nul
-echo Accediendo a camara web...
-ping localhost -n 2 >nul  
-echo Monitorizando llamadas telefonicas...
-ping localhost -n 2 >nul
-echo Leyendo historial de navegacion...  
-ping localhost -n 2 >nul
+ping localhost -n 2 > nul
+echo Descifrando contraseñas...
+ping localhost -n 2 > nul
+echo Accediendo a la cámara web...
+ping localhost -n 2 > nul
+echo Monitorizando llamadas telefónicas...
+ping localhost -n 2 > nul
+echo Leyendo historial de navegación...
+ping localhost -n 2 > nul
 echo Extrayendo fotos y archivos...
-ping localhost -n 3 >nul
+ping localhost -n 2 > nul
 echo Enviando datos a servidor secreto...
-ping localhost -n 3 >nul
+ping localhost -n 2 > nul
 echo.
-echo ^<Todos sus datos me pertenecen ahora! Muajaja!^>
-echo ^<Le estare vigilando...^>
+echo Todos sus datos me pertenecen ahora jeje
+echo Estaré vigilándolo...
 goto finalizar
 
 :cancelado
 echo Operación cancelada.
 
-:finalizar 
-pause >nul
+:finalizar
+pause
